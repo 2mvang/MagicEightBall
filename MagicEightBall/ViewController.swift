4730ed736aaf284392dcb1924755ca4fa6a69c3c
MVang <minimevang@gmail.com>
2020-01-21 15:17:15 -0800

    Initial Commit

---
+++ MagicEightBall/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by Marissa Vang on 1/21/20.
+//  Copyright © 2020 Marissa Vang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
